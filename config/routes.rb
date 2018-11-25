a72017607d2cc7ee5884c5a1ed5afd8e94b1850c
linguaCodici <victoriqueee@gmail.com>
2018-11-24 21:44:09 -0600

    updated main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  root "ui#index"
+
   scope :api, defaults: {format: :json} do
     # Ex:- scope :active, -> {where(:active => true)}
     resources :foos, except: [:new, :edit]
     resources :bars, except: [:new, :edit]
   end
+
+  get 'ui' => 'ui#index'
+  get 'ui#' => 'ui#index'
 end
